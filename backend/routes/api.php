6e8ec5461d01db7ec735a5fc24edb35cf3438af9
cbmella <cbuguenomella@gmail.com>
2023-09-07 14:22:35 -0300

    Configure JWT authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+Route::post('logout', 'AuthController@logout')->middleware('auth:api');
+
+Route::group(['middleware' => ['auth:api']], function () {
+    // Tus rutas protegidas aquí
+});
