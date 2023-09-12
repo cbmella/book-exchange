fa4c6d5cd3f9ec393afd91940a5eae3d430b5c13
cbmella <cbuguenomella@gmail.com>
2023-09-12 00:59:32 -0300

    feat(models): define Exchange eloquent relationships

--- backend/app/Models/Exchange.php
+++ backend/app/Models/Exchange.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Exchange extends Model
 {
     use HasFactory;
+
+    public function borrower() {
+        return $this->belongsTo(User::class, 'borrower_id');
+    }
+
+    public function lender() {
+        return $this->belongsTo(User::class, 'lender_id');
+    }
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
 }
