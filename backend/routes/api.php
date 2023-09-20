a8bffdc4d438b4c15e7ac3cd7c75593e0e3a7fa3
cbmella <cbuguenomella@gmail.com>
2023-09-20 16:27:45 -0300

    fix(routes): remove duplicated routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -33,31 +33,3 @@ Route::group(['middleware' => ['auth:api']], function () {
      */
     Route::post('exchanges/{exchange}/review', [ReviewController::class, 'store'])->name('reviews.store');
 });
-/**
- * Authentication routes
- */
-Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('register', [AuthController::class, 'register'])->name('register');
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
-
-/**
- * Routes that require authentication
- */
-Route::group(['middleware' => ['auth:api']], function () {
-    /**
-     * Book routes
-     */
-    Route::get('books/search', [BookController::class, 'search'])->name('books.search');
-    Route::resource('books', BookController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
-
-    /**
-     * Exchange routes
-     */
-    Route::post('books/{book}/request-exchange', [ExchangeController::class, 'requestExchange'])->name('books.request-exchange');
-    Route::patch('exchanges/{exchange}/accept', [ExchangeController::class, 'acceptExchange'])->name('exchanges.accept');
-
-    /**
-     * Review routes
-     */
-    Route::post('exchanges/{exchange}/review', [ReviewController::class, 'store'])->name('reviews.store');
-});
