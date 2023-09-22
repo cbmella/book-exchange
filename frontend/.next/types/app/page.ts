d71f6b05b56c37cbcb85c8da7f080a437686cf00
cbmella <cbuguenomella@gmail.com>
2023-09-22 13:11:47 -0300

    ok

--- frontend/.next/types/app/page.ts
+++ frontend/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /media/cesar/SSD/Projects/book-exchange/frontend/src/app/page.tsx
+// File: /app/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
