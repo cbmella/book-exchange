1898662d6d75c86037c9c2f307263a8fb63f6aad
cbmella <cbuguenomella@gmail.com>
2023-09-26 22:19:26 -0300

    fix permissions

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,15 @@
 # Instala dependencias con Composer
 composer install
 
+# Si el archivo .env no existe, copia el .env.example y genera la clave de aplicación
+if [ ! -f "/var/www/html/.env" ]; then
+    cp /var/www/html/.env.example /var/www/html/.env
+    php /var/www/html/artisan key:generate
+fi
+
+# Establecer permisos para los directorios de Laravel
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Start php-fpm in the background
 php-fpm &
 
