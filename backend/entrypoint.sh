8f292b55bb3687151418e172501182b586e7e6e5
cbmella <cbuguenomella@gmail.com>
2023-10-02 20:27:48 -0300

    ok

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,10 +4,15 @@
 composer install
 
 # Si el archivo .env no existe, copia el .env.example y genera la clave de aplicación
+echo "Verificando la existencia de .env..."
 if [ ! -f "/var/www/html/.env" ]; then
+    echo ".env no encontrado, copiando de .env.example..."
     cp /var/www/html/.env.example /var/www/html/.env
     php /var/www/html/artisan key:generate
+else
+    echo ".env ya existe."
 fi
+
 
 # Establecer permisos para los directorios de Laravel
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
